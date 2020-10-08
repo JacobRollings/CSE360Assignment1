eb677098e79e7264056bc4f8ab084d95a7790bc6
jaked <jnrollin@asu.edu>
2020-10-07 21:11:28 -0700

    First Stage of CSE 360 Assignment 1

---
+++ CSE360Assignment1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,42 @@
+/*
+ *Author: Jacob Rollings
+ *File name: AddingMachine.java
+ *Description: Performs addition and subtraction, keeps a running total, and prints
+ *             out the operations done to the total.
+ */
+
+package cse360assignment02;			//Includes this file in the cse360assignment02
+
+public class AddingMachine {		//beginning of the AddingMachine Class
+	private int total; 				//Create a private variable named total
+	
+	public AddingMachine() 			//Start of the AddingMachine Constructor
+	{
+		total = 0;					//Set total to 0 initially
+	}
+	
+	public int getTotal()			//Start of the getTotal method
+	{
+		return 0;					//Return 0 initially, this will be changed later
+	}
+	
+	public void add(int value)		//Start of the add method that takes an int value as a parameter
+	{
+		
+	}
+	
+	public void subtract(int value)	//Start of the subtract method that takes an int value as a parameter
+	{
+		
+	}
+	
+	public String toString()		//Start of the toString method that returns the string of the manipulations to total
+	{
+		return "";
+	}
+	
+	public void clear()				//Start of the clear method
+	{
+		
+	}
+}
